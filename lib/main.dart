1a85356bcf09bd15388fafd081bed39418aac3a6
Gustavo Jhon <hustavojhon@gmail.com>
2023-03-31 16:18:47 -0500

    📱 Inicializando mi proyecto con Flutter

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const Color darkBlue = Color.fromARGB(255, 37, 39, 40);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'PIRATE',
+      style: Theme.of(context).textTheme.headline4,
+    );
+  }
+}
